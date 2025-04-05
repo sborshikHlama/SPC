3399eaf738479623e6fbd99be8d2f3c7e7d9572e
Arsenii Evstigneev <logrus.928@gmail.com>
2025-04-05 18:18:23 +0200

    add Cusum chart

---
+++ CUSUM.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import kagglehub
+import os
+
+path = kagglehub.dataset_download("satvicoder/call-center-data")
+
+files = os.listdir(path)
+filename = next(f for f in files if f.endswith(".csv"))
+ful_path = os.path.join(path, filename)
+df = pd.read_csv(ful_path)
+cusum_df = df.head(60).copy()
+cusum_df['Answer Speed (Seconds)'] = pd.to_timedelta(cusum_df['Answer Speed (AVG)']).dt.total_seconds()
+cusum_df = cusum_df.dropna(subset=['Answer Speed (Seconds)'])
+
+target = cusum_df['Answer Speed (Seconds)'].mean()
+sigma = cusum_df['Answer Speed (Seconds)'].std()
+k = 0.5 * sigma
+H = 5 * sigma
+
+cusum_df['C+'] = 0.0
+cusum_df['C-'] = 0.0
+for i in range(1, len(cusum_df)):
+    x = cusum_df.loc[cusum_df.index[i], 'Answer Speed (Seconds)']
+    cusum_df.loc[cusum_df.index[i], 'C+'] = max(0, cusum_df.loc[cusum_df.index[i - 1], 'C+'] + (x - target - k))
+    cusum_df.loc[cusum_df.index[i], 'C-'] = max(0, cusum_df.loc[cusum_df.index[i - 1], 'C-'] + (target - x - k))
+    
+m = 10
+k = 0.5 * sigma
+xc = len(cusum_df)
+yc = cusum_df['C+'].iloc[-1]
+
+v_mask_x = [xc - m, xc, xc - m]
+v_mask_y = [yc + k * m, yc, yc - k * m]
+
+plt.figure(figsize=(14, 6))
+plt.plot(range(1, len(cusum_df) + 1), cusum_df['C+'], color='red', label='C+ (накопление превышения)')
+plt.plot(range(1, len(cusum_df) + 1), cusum_df['C-'], color='blue', label='C- (накопление снижения)')
+plt.axhline(H, linestyle='--', color='black', label='Контрольный предел H')
+plt.title('CUSUM-график: Среднее время ответа')
+plt.plot(v_mask_x, v_mask_y, linestyle='--', color='black', label='V-маска')
+plt.xlabel('День')
+plt.ylabel('Накопленное отклонение (сек.)')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
